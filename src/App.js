d6d777aeaea79ea3e7cda1b8552a956a8147cd4b
Aditi0505 <aditi5may1997@gmail.com>
2022-05-16 01:13:37 +0530

    feat: add wallpaper context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { UserOnboarding } from "./pages";
 import "./App.css";
-
+import { WallpaperProvider } from "./context/wallpaper-context";
 function App() {
-  return <h1 className="underline">Hello</h1>;
+  return (
+    <WallpaperProvider>
+      <Routes>
+        <Route path="/" exact element={<UserOnboarding />} />
+      </Routes>
+    </WallpaperProvider>
+  );
 }
 
 export default App;
---
+++ src/context/index.js
@@ -0,0 +1,1 @@
+export { useWallpaper, WallpaperProvider } from "./wallpaper-context";
