25ac1841b0eb5f23d7221d2219a486cda53fd78e
Aditi0505 <aditi5may1997@gmail.com>
2022-05-17 01:30:14 +0530

    add: fonts to the pages

--- src/pages/User-OnBoard/UserOnboarding.jsx
+++ src/pages/User-OnBoard/UserOnboarding.jsx
@@ -23,13 +23,13 @@ const UserOnboarding = () => {
       className="flex flex-col justify-center items-center text-6xl text-white h-screen w-full bg-cover bg-no-repeat bg-clip-border bg-fixed bg-center"
       style={wallpaper}
     >
-      <div className="w-full text-center">
+      <div className="w-full text-center font-Montserrat font-bold">
         <header>
           <h1>Hello! What's your name?</h1>
         </header>
         <main>
           <input
-            className="text-center font-extrabold text-7xl w-11/12 py-4 px-0 border-0 border-b-4 outline-0 bg-transparent"
+            className="text-center font-Quattrocento font-extrabold text-7xl w-11/12 py-4 px-0 border-0 border-b-4 outline-0 bg-transparent"
             onChange={(e) =>
               userDispatch({
                 type: "ADD_USER",
